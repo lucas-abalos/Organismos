8f60868be403a18a850d1c0013fdf96dbdcae0b3
lucas-abalos <lucas22abalos@gmail.com>
2024-12-30 12:24:31 -0300

    nuevo commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import MainPage from './pages/mainPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import NoTAMSPage from './pages/noTAMSPage';
 
 const theme = createTheme({
@@ -28,7 +28,7 @@ const App: React.FC = () => {
         <Router>
           <Routes>
             {/* Ruta principal */}
-            <Route path="/" element={<MainPage />} />
+            <Route path="/" element={<Navigate to="/Organismos" replace />} />
             <Route path="/Organismos" element={<MainPage />} />
             {/* Ruta para la pantalla de NOTAMS */}
             <Route path="/notams" element={<NoTAMSPage />} />
